18eee2f573ab98bd907f71b39c7f9ef0ecf7efda
mukhammadsyafiqulamin <114373975+aminpgt23@users.noreply.github.com>
2026-03-30 22:17:05 +0700

    Update database connection configuration
    Removed default values for DB host, port, and user.

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'itjobs-mukhammadsyafiqulamin-69d7.h.aivencloud.com',
-  port: process.env.DB_PORT || 23587,
-  user: process.env.DB_USER || 'avnadmin',
+  host: process.env.DB_HOST || '',
+  port: process.env.DB_PORT || ,
+  user: process.env.DB_USER || '',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'production',
   waitForConnections: true,
