b8a6103997c9d9a3b1b3ea27f1fc8e94eaf079f4
mukhammadsyafiqulamin <114373975+aminpgt23@users.noreply.github.com>
2026-03-30 21:37:43 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
+  host: process.env.DB_HOST || 'itjobs-mukhammadsyafiqulamin-69d7.h.aivencloud.com',
+  port: process.env.DB_PORT || 23587,
+  user: process.env.DB_USER || 'avnadmin',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'production',
   waitForConnections: true,
